5409c3eace4d87fa4b232fcb0c6bf936ab3f7de0
rahfathoni <rahfathoni@gmail.com>
2025-09-14 00:51:58 +0700

    feat(): add notavailable; update menu and notfound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,9 @@
 export default function NotFound() {
   return (
-    <div className="p-4 text-center">
-      <h1 className="text-2xl font-bold text-danger">404 - Page Not Found</h1>
+    <div className="flex flex-col items-center justify-center min-h-[60vh] p-4 text-center">
+      <h1 className="text-2xl font-bold text-danger mb-6">
+        404 - Page Not Found
+      </h1>
     </div>
   );
 }
